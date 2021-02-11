0e8bae96ae78aa76dadb544542071c5104df43ef
Wallace <wbescrovaine@gmail.com>
2021-02-11 14:39:16 -0300

    Working
    yarn install

---
+++ src/config/multer.js
@@ -0,0 +1,44 @@
+// Imports
+const multer = require('multer');
+const path = require('path');
+const crypto = require('crypto');
+
+// Configurações do multer
+
+module.exports = {
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
+    },
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        console.log(file)
+        console.log(file.originalname)
+
+        const fileName = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, fileName);
+      });
+    },
+  }),
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'application/pdf',
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type.'));
+    }
+  },
+};
